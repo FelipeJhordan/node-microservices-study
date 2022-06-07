8658a90b2e6ce554137ed5a0aaac45d750a43c50
felipejhordan <felipejordan.alves@gmail.com>
2022-06-07 12:44:18 -0300

    feat: create a category controller and category module ( empty controllers or providers, but not should be )

--- iniciando-microservices-nestjs/nest-microservices-study-1/src/category/category.controller.ts
+++ iniciando-microservices-nestjs/nest-microservices-study-1/src/category/category.controller.ts
@@ -1,9 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Render, Req } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
 
 @Controller('category')
 export class CategoryController {
-  @Get()
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepo: Repository<Category>,
+  ) {}
+
+  @Get() //API Rest - JSON
   index() {
-    return 'Categories';
+    return this.categoryRepo.find();
+  }
+
+  @Get('list')
+  @Render('category_list')
+  async category_list() {
+    const categories = await this.categoryRepo.find();
+    return { layout: false, categories };
+  }
+
+  @Get('create')
+  async category_create() {
+    const category = await this.categoryRepo.create({ name: 'category teste' });
+    return this.categoryRepo.save(category);
+  }
+
+  @Post()
+  async store(@Req() request: Request) {
+    const category = await this.categoryRepo.create(request.body as any);
+    return this.categoryRepo.save(category);
   }
 }
