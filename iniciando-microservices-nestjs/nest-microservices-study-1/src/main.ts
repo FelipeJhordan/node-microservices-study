82186f3a95bb916bc9a05f63489b4a8fcbdaf7fd
felipejhordan <felipejordan.alves@gmail.com>
2022-06-07 12:42:24 -0300

    feat: configure handlerbars and set public folder

--- iniciando-microservices-nestjs/nest-microservices-study-1/src/main.ts
+++ iniciando-microservices-nestjs/nest-microservices-study-1/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as expressHbs from 'express-handlebars';
+import * as handlebarsHelpers from 'handlebars-helpers';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const hbs = expressHbs.create({
+    helpers: handlebarsHelpers(),
+    extname: '.hbs',
+    partialsDir: [join(__dirname, '..', 'views')],
+  });
+
+  app.engine('hbs', hbs.engine);
+  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(3000);
 }
 bootstrap();
